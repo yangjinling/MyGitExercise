315f70895346d41ee6ed9d1c7299b2dfa70527cc
yangjinling <18363820101@163.com>
2016-05-31 13:32:48 +0800

    仿京东、淘宝跑马灯属性设置

--- paomadeng/src/main/java/com/example/paomadeng/MainActivity.java
+++ paomadeng/src/main/java/com/example/paomadeng/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.paomadeng;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.paomadeng.view.TextViewAd;
 
@@ -19,10 +20,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         tv = ((TextViewAd) findViewById(R.id.tv));
         lists=new ArrayList<ADEnity>();
-        lists.add(new ADEnity("1:","1",""));
-        lists.add(new ADEnity("2:","2",""));
-        lists.add(new ADEnity("3:","3",""));
-        lists.add(new ADEnity("4:","4",""));
+        lists.add(new ADEnity("吃饭注意的事项:","细嚼慢咽","1111111111111"));
+        lists.add(new ADEnity("京东最大的优势:","自营","222222222222"));
+        lists.add(new ADEnity("你是谁？","我是电脑","333333333333"));
+        lists.add(new ADEnity("他是谁？","他是手机","444444444444"));
         tv.setmTexts(lists);
+        tv.setOnClickLitener(new TextViewAd.onClickLitener() {
+            @Override
+            public void onClick(String mUrl) {
+                Toast.makeText(MainActivity.this,""+mUrl,Toast.LENGTH_SHORT).show();
+            }
+        });
+        tv.setmDuration(1500);
+        tv.setmInterval(500);
+        tv.setFrontColor(R.color.colorAccent);
+        tv.setFrontColor(R.color.colorPrimary);
     }
 }
