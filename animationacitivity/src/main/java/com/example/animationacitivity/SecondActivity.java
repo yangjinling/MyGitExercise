4472226597733e7bc7b0444295129b06bf4e9c90
yangjinling <18363820101@163.com>
2016-06-01 08:50:51 +0800

    线程池

--- animationacitivity/src/main/java/com/example/animationacitivity/SecondActivity.java
+++ animationacitivity/src/main/java/com/example/animationacitivity/SecondActivity.java
@@ -3,11 +3,23 @@ package com.example.animationacitivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class SecondActivity extends AppCompatActivity {
+
+    private ExecutorService mExecutorService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_activiti);
+        mExecutorService = Executors.newSingleThreadExecutor();
+        mExecutorService.execute(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
     }
 }
